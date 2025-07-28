693d0d9b492d23f2b142a26221ddc681ddda89a8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2025-07-29 01:52:36 +0600

    husky-test

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Hello, TypeScript Express Server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+const asss = 44;
